bce5de520e1773ea3856ce6df49002532c8d82a6
zidamn <nonname@gmail.com>
2023-12-07 21:56:24 +0800

    Add files via upload
    fix multiple payload issue

--- update_rule.ps1
+++ update_rule.ps1
@@ -25,10 +25,48 @@ $customPart = $oldContent[($beginLine-1)..($endLine-1)]
 $customPartString = $customPart -join "`r`n"
 #$customPart = $oldContent[$beginLine..$endLine]
 
-# Retrieve the content from the URLs and merge them
-$mergedContent = foreach ($url in $urls) {
+# Retrieve the content from the URLs, remove the line with "payload:", and merge them
+<# $$ = foreach ($url in $urls) {
     Invoke-WebRequest -Uri $url | Select-Object -ExpandProperty Content
+} #>
+
+# Loop through each URL and retrieve the data
+foreach ($url in $urls) {
+	try{
+		$content = Invoke-WebRequest -Uri $url -UseBasicParsing | Select-Object -ExpandProperty Content
+
+		# Remove lines containing "payload:"
+		$content = $content -split "`n" | Where-Object { $_ -notmatch '^\s*payload' }
+
+		# Add the modified content to the array
+		$contentArray += $content
+	}
+	catch {
+		Write-Error "An error occurred while processing $url"
+    }
 }
+$mergedContent = $contentArray -join "`r`n"
+
+
+<# foreach ($url in $urls) {
+    try {
+        # Retrieve text data from the URL
+        $content = Invoke-WebRequest -Uri $url -UseBasicParsing | Select-Object -ExpandProperty Content
+
+        # Split content into individual lines
+        $lines = $content -split "`n"
+
+        # Remove any lines containing "payload:"
+        $filteredLines = $lines | Where-Object { $_ -notmatch "^(payload:).*" }
+
+        # Append filteredLines to the StringBuilder object, add a newline between files
+        [void]$mergedData.AppendLine(($filteredLines -join "`n") + "`n")
+    }
+    catch {
+        Write-Error "An error occurred while processing $url"
+    }
+}
+$mergedContent = $mergedData.ToString() #>
 
 # Get the current timestamp
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
